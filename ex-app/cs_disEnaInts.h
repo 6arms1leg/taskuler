f15bb38d3970f809da3ec0fdedcd74b0e60f40b2
6arms1leg <m.brnsfld@googlemail.com>
2022-01-02 20:19:47 +0100

    Complete example

--- ex-app/cs_disEnaInts.h
+++ ex-app/cs_disEnaInts.h
@@ -1,15 +1,18 @@
 #ifndef CS_DISENAINTS_H
 #define CS_DISENAINTS_H
 
+/* Include required interfaces here, e.g. `#include <interruptInterface.h>` */
+#include <avr/interrupt.h> /* Provides AVR MCU interrupt handling */
+
 /**
  * \brief User-defined macros to dis-/enable all relevant interrupts
  *
  * \{
  */
-#define CS_DISINTS /* Add code to disable relevant interrupts here, e.g.
-                      `( disableAllInterrupts(); )` */
-#define CS_ENAINTS /* Add code to enable relevant interrupts here, e.g.
-                      `( enableAllInterrupts(); )` */
+#define CS_DISINTS cli(); /* Add code to disable relevant interrupts here, e.g.
+                             `disableAllInterrupts();` */
+#define CS_ENAINTS sei(); /* Add code to enable relevant interrupts here, e.g.
+                             `enableAllInterrupts();` */
 /** \} */
 
 #endif /* CS_DISENAINTS_H */
